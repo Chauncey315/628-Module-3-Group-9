d8b7c672a030c28b6a9d8f372bc357272fe40f64
Yezhou Li <yli967@wisc.edu>
2019-11-16 15:32:34 -0600

    Map Adjustment

--- Shiny/app.R
+++ Shiny/app.R
@@ -10,7 +10,7 @@ ui <- navbarPage(
     "Yelp Business Analysis", id = "nav",
     # theme = "superZip.css",
     
-    tabPanel("Interactive map",
+    tabPanel("Interactive Map",
              div(class="outer",
                  tags$head(
                      includeCSS("www/superZip.css"),
@@ -20,41 +20,46 @@ ui <- navbarPage(
                  leafletOutput("map", width="100%", height="100%"),
                  
                  absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                               draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                               draggable = TRUE, top = 130, left = 10, bottom = "auto",
                                width = 330, height = "auto",
+                               # right = 20,
                                
-                               h2("Restaurant explorer"),
+                               h2("Restaurant Explorer"),
                                
                                textInput(inputId = "search", placeholder = "Search for something",
                                          label = "Search"),
                                
-                               checkboxGroupInput(inputId = "cuisine", label = "Cuisine",
-                                                  choices = cuisineSet, selected = cuisineSet ),
-                               
                                sliderInput(inputId = "starRange", label = "Star", round = starPrecision,
-                                           min = 0., max = 5., value = c(0, 5), step = 0.1**starPrecision)
+                                           min = 0., max = 5., value = c(0., 5.), step = 0.1**starPrecision),
+                               
+                               checkboxGroupInput(inputId = "cuisineSet", label = "Cuisine",
+                                                  choices = cuisineSet, selected = cuisineSet)
                  ),
              ),
-    )
+    ),
+    
+    tabPanel("Network Analysis")
 )
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session){
-    locFiltered = reactive(filterLocation( input$cuisine, input$starRange))
+    locFiltered = reactive(filterLocation( input$cuisineSet, input$starRange))
+    
+    
     
     output$map = renderLeaflet({
         leaflet() %>% 
             addTiles() %>% 
-            setView(-79.40229, 43.73350, 14) %>% 
+            setView(-79.40229, 43.73350, 10) %>% 
             addCircles(locFiltered()$longitude, locFiltered()$latitude, weight = 3, radius=20, 
                        color=locFiltered()$color, stroke = TRUE, fillOpacity = 0.8) %>% 
             addMarkers(locFiltered()$longitude, locFiltered()$latitude, clusterOptions = markerClusterOptions(),
                        clusterId = "cluster1")
     })
     
-    output$slider = renderText(str(input$starRange))
-    output$cuisine = renderText(str(input$cuisine))
+    output$starRangeslider = renderText(str(input$starRange))
+    output$cuisineSetCheckbox = renderText(str(input$cuisineSet))
     
 }
 
